20f4251b4854a89f3c818e5649c33f77e77590eb
Paul Isoard <paulisoard2911@gmail.com>
2023-03-07 15:05:55 +0100

    MongoDB password change

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 try {
-    const url = process.env.MONGO_URI
+    const url = process.env.MONGODB_URI
     mongoose.connect(url)
     console.log('connected to MonPetitSurf_DB')
 }
